dd18763313ef8afd1eeab71c66d10869647bba59
Tatyana Scotce <tscotce@gmail.com>
2018-03-13 22:38:34 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ class Anagram
   def match(list)
     list.collect do |w|
       if w.split("").sort! == @word.split("").sort! 
-      return w.to_a
+      #return w.to_a
       end
     return []  
     end
